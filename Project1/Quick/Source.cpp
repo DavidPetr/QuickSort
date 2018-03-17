c998cdc3428411e7f5425aa11da04aa2695633a2
DavidPetr <petersen1999@mail.ru>
2018-03-17 23:44:40 +0400

    Quick Sort Initialization

---
+++ Project1/Quick/Source.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+void quick(int [], int, int);
+int main() {
+	const int n = 6;
+	int a[n] = { 10,7,8,9,1,5};
+
+	quick(a, 0, n);
+	for (int i = 0; i < n; i++) {
+		cout << a[i] << endl;
+	}
+	system("PAUSE");
+	return 0;
+}
+void quick(int a[], int low, int high) {
+	int max = a[high - 1],i = low;
+	for (int j = low; j < high; j++)
+		if (a[j] <= max) {
+			int t = a[i];
+			a[i] = a[j];
+			a[j] = t;
+			/*
+			a[i] ^= a[j];
+			a[j] ^= a[i];
+			a[i] ^= a[j];
+			*/
+			i++;
+		}
+	if (i == high)return;
+	quick(a, low, i);
+	quick(a, i, high);
+}
